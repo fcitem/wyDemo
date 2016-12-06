1a85dab5f21e6c2f997ed60e46a516cac02ecfea
fc <13297903879@163.com>
2016-12-06 20:48:35 +0800

    同步，消除冲突

--- wyDemo/src/main/java/com/wyDemo/util/Md5Url.java
+++ wyDemo/src/main/java/com/wyDemo/util/Md5Url.java
@@ -1,11 +1,8 @@
 package com.wyDemo.util;
 
 import java.net.URLEncoder;
-import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Iterator;
-
-import sun.misc.BASE64Encoder;
 
 /**
  * @author fengchao
@@ -14,13 +11,22 @@ import sun.misc.BASE64Encoder;
  */
 public class Md5Url {
 	private final static String  consumerKey="18462081";
-	private static MessageDigest md5=null;
+	private final static String  secretKey="qfMX8XnovK9CFHbf";
+	private static long timestamp;
 	private static String md5Url=null;
+	public static long getTimestamp() {
+		return timestamp;
+	}
+	public static void setTimestamp(long timestamp) {
+		Md5Url.timestamp = timestamp;
+	}
 	public static String encodeUrl(String url,String requestMethord,HashMap<String,Object> map){
 		StringBuilder builder=new StringBuilder();
 		builder.append(requestMethord);
 		builder.append(url);
-		builder.append("timestamp="+System.currentTimeMillis());
+		timestamp=System.currentTimeMillis();
+		builder.append("timestamp="+timestamp);
+		builder.append(secretKey);
 		builder.append("consumerKey="+consumerKey);
 		Iterator<String> it=map.keySet().iterator();
 		while(it.hasNext()){
@@ -28,11 +34,11 @@ public class Md5Url {
 			builder.append(key).append("=").append(map.get(key));
 		}
 		try {
-			md5=MessageDigest.getInstance("MD5");
+//			md5=MessageDigest.getInstance("MD5");
 			String encodeUrl=(URLEncoder.encode(builder.toString(), "UTF-8"));
-			BASE64Encoder base64en = new BASE64Encoder();
+//			BASE64Encoder base64en = new BASE64Encoder();
 			//加密后的字符串
-			md5Url=base64en.encode(md5.digest(encodeUrl.getBytes("utf-8")));
+			md5Url=MD5.getInstance().getMD5(encodeUrl);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
